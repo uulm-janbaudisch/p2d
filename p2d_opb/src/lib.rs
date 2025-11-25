c8ffee69c2a6d6f3b7a0722c867b321118cc6de2
Jan Baudisch <jan.baudisch@uni-ulm.de>
2025-11-25 12:12:48 +0100

    feat(opb): enable serializtation

--- p2d_opb/src/lib.rs
+++ p2d_opb/src/lib.rs
@@ -1,6 +1,7 @@
 mod parser;
 
 pub use parser::parse;
+use std::fmt::{Display, Formatter};
 
 use bimap::{BiHashMap, BiMap};
 
@@ -23,11 +24,38 @@ impl OPBFile {
         }
     }
 }
+
+impl Display for OPBFile {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "* #variable= {} #constraint= {}",
+            self.number_variables, self.number_constraints
+        )?;
+        self.equations
+            .iter()
+            .map(|equation| equation.to_string(&self.name_map))
+            .try_for_each(|equation| writeln!(f, "{equation}"))
+    }
+}
+
 #[derive(Clone)]
 pub struct Equation {
     pub lhs: Vec<Summand>,
     pub rhs: i128,
     pub kind: EquationKind,
+}
+
+impl Equation {
+    pub fn to_string(&self, variable_map: &BiMap<String, u32>) -> String {
+        let lhs = self.lhs.iter().fold(String::new(), |mut output, summand| {
+            output.push_str(summand.to_string(variable_map).as_str());
+            output.push(' ');
+            output
+        });
+
+        format!("{}{} {};", lhs, self.kind, self.rhs)
+    }
 }
 
 #[derive(PartialEq, Debug, Clone)]
@@ -40,9 +68,60 @@ pub enum EquationKind {
     NotEq,
 }
 
+impl Display for EquationKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EquationKind::Eq => write!(f, "="),
+            EquationKind::Ge => write!(f, ">="),
+            EquationKind::Le => write!(f, "<="),
+            EquationKind::G => write!(f, ">"),
+            EquationKind::L => write!(f, "<"),
+            EquationKind::NotEq => write!(f, "!="),
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct Summand {
     pub variable_index: u32,
     pub factor: i128,
     pub positive: bool,
+}
+
+impl Summand {
+    pub fn to_string(&self, variable_map: &BiMap<String, u32>) -> String {
+        let mut output = format!("{} ", self.factor);
+
+        if !self.positive {
+            output.push('-')
+        }
+
+        output.push_str(
+            variable_map
+                .get_by_right(&self.variable_index)
+                .expect("variable not found"),
+        );
+
+        output
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::parse;
+
+    #[test]
+    fn parse_and_display() {
+        let input = r#"#variable= 7 #constraint= 2
+x + 2 a + b + c >= 3;
+-1 d + e + 1 * f >= 1;"#;
+
+        let expected = r#"* #variable= 7 #constraint= 2
+1 x 2 a 1 b 1 c >= 3;
+-1 d 1 e 1 f >= 1;
+"#;
+
+        let parsed = parse(input).expect("failed to parse input");
+        assert_eq!(parsed.to_string(), expected);
+    }
 }
